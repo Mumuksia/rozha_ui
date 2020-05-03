48fe7a0e81e4e0810df60179b20fbd534dcb00cb
Mumuksia <iurii.zavada@wirelesscar.com>
2020-05-03 17:03:16 +0200

    Updates

---
+++ precache-manifest.6aeb8c081aa1ad4b7f9b36bb5f04e258.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30eb68ef7222188c314f8bc347535ae5",
+    "url": "/rozha_ui/index.html"
+  },
+  {
+    "revision": "584cdd7a93a04632a4f5",
+    "url": "/rozha_ui/static/css/2.e8d0343d.chunk.css"
+  },
+  {
+    "revision": "4e4ee0be16bd55023c8a",
+    "url": "/rozha_ui/static/css/main.abd25b8d.chunk.css"
+  },
+  {
+    "revision": "584cdd7a93a04632a4f5",
+    "url": "/rozha_ui/static/js/2.b104cb3f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/rozha_ui/static/js/2.b104cb3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e4ee0be16bd55023c8a",
+    "url": "/rozha_ui/static/js/main.0e4bbecb.chunk.js"
+  },
+  {
+    "revision": "11f706382718cd0a9c06",
+    "url": "/rozha_ui/static/js/runtime-main.c460a5cc.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/rozha_ui/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/rozha_ui/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/rozha_ui/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/rozha_ui/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/rozha_ui/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/rozha_ui/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/rozha_ui/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/rozha_ui/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/rozha_ui/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
